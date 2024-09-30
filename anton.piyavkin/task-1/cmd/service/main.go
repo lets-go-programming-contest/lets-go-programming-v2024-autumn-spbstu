c60959d415a1fafc95fafaa30906820d4b2f53a1
Piyavva <piyavkin05@bk.ru>
2024-09-30 17:04:32 +0300

    Add calculate

--- anton.piyavkin/task-1/cmd/service/main.go
+++ anton.piyavkin/task-1/cmd/service/main.go
@@ -5,9 +5,21 @@ import (
 	"os"
 )
 
+func calculate(lhs int, rhs int, op string) int {
+    switch op {
+	case "+":
+		return lhs + rhs
+	case "-":
+    	return lhs - rhs
+	case "/":
+		return lhs / rhs
+    }
+	return lhs * rhs
+}
+
 func main() {
     fmt.Print("Введите первое число:")
-	var lhs int64
+	var lhs int
 	_, err := fmt.Scan(&lhs)
 	if err != nil {
 		fmt.Fprint(os.Stderr, "Некорректное число. Пожалуйста, введите числовое значение.\n")
@@ -20,11 +32,12 @@ func main() {
 		fmt.Fprint(os.Stderr, "Некорректная операция. Пожалуйста, используйте символы +, -, * или /.\n")
 		return
 	}
-	var rhs int64
+	var rhs int
 	fmt.Print("Введите второе число:")
 	_, err = fmt.Scan(&rhs)
 	if err != nil || (op == "/" && rhs == 0) {
 		fmt.Fprint(os.Stderr, "Некорректное число. Пожалуйста, введите числовое значение.\n")
 		return
 	}
+    fmt.Println("Результат:", lhs, op, rhs, "=", calculate(lhs, rhs, op))
 }
