c5754cb24c4088ed5c65a2712e4eb47bcb3d6d61
Piyavva <piyavkin05@bk.ru>
2024-09-30 17:18:50 +0300

    Fix spaces and tabs

--- anton.piyavkin/task-1/cmd/service/main.go
+++ anton.piyavkin/task-1/cmd/service/main.go
@@ -8,36 +8,36 @@ import (
 func calculate(lhs int, rhs int, op string) int {
     switch op {
 	case "+":
-		return lhs + rhs
+        return lhs + rhs
 	case "-":
-    	return lhs - rhs
+        return lhs - rhs
 	case "/":
-		return lhs / rhs
+        return lhs / rhs
     }
 	return lhs * rhs
 }
 
 func main() {
     fmt.Print("Введите первое число:")
-	var lhs int
-	_, err := fmt.Scan(&lhs)
-	if err != nil {
-		fmt.Fprint(os.Stderr, "Некорректное число. Пожалуйста, введите числовое значение.\n")
-		return
-	}
-	fmt.Print("Выберите операцию (+, -, *, /):")
-	var op string
-	_, err = fmt.Scan(&op)
-	if err != nil || (op != "+" && op != "*" && op != "-" && op != "/") {
-		fmt.Fprint(os.Stderr, "Некорректная операция. Пожалуйста, используйте символы +, -, * или /.\n")
-		return
-	}
-	var rhs int
-	fmt.Print("Введите второе число:")
-	_, err = fmt.Scan(&rhs)
-	if err != nil || (op == "/" && rhs == 0) {
-		fmt.Fprint(os.Stderr, "Некорректное число. Пожалуйста, введите числовое значение.\n")
-		return
-	}
+    var lhs int
+    _, err := fmt.Scan(&lhs)
+    if err != nil {
+    	fmt.Fprint(os.Stderr, "Некорректное число. Пожалуйста, введите числовое значение.\n")
+    	return
+    }
+    fmt.Print("Выберите операцию (+, -, *, /):")
+    var op string
+    _, err = fmt.Scan(&op)
+    if err != nil || (op != "+" && op != "*" && op != "-" && op != "/") {
+    	fmt.Fprint(os.Stderr, "Некорректная операция. Пожалуйста, используйте символы +, -, * или /.\n")
+    	return
+    }
+    var rhs int
+    fmt.Print("Введите второе число:")
+    _, err = fmt.Scan(&rhs)
+    if err != nil || (op == "/" && rhs == 0) {
+    	fmt.Fprint(os.Stderr, "Некорректное число. Пожалуйста, введите числовое значение.\n")
+    	return
+    }
     fmt.Println("Результат:", lhs, op, rhs, "=", calculate(lhs, rhs, op))
 }
