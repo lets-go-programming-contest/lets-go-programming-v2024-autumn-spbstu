5e8251f28b8c316754c3598301f9621d92941e42
Piyavva <piyavkin05@bk.ru>
2024-10-02 16:51:32 +0300

    [TASK-2] Add function get K element

--- anton.piyavkin/task-2/cmd/service/main.go
+++ anton.piyavkin/task-2/cmd/service/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func getKel(arr []int, k int) int {
+	var tmp []int = []int{}
+	mn := math.MaxInt
+	for i := 0; i < k; i++ {
+		mn = min(mn, arr[i])
+		tmp = append(tmp, arr[i])
+	}
+	for i := k; i < len(arr); i++ {
+		if mn < arr[i] {
+			var temp int = arr[i]
+			for j := 0; j < len(tmp); j++ {
+				if mn == tmp[j] {
+					tmp[j] = arr[i]
+					mn = arr[i]
+				} else {
+					temp = min(temp, tmp[j])
+				}
+			}
+			mn = temp
+		}
+	}
+	return mn
+}
+
+func main() {
+    var arr []int = []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
+	fmt.Println(getKel(arr, 4))
 }
