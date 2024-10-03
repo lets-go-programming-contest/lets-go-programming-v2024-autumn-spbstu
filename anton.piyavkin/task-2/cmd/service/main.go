4bbfb9f6e25d934b6f28f3e0e7f18fcd9224a21f
Piyavva <piyavkin05@bk.ru>
2024-10-03 17:49:51 +0300

    [TASK-2] Add input

--- anton.piyavkin/task-2/cmd/service/main.go
+++ anton.piyavkin/task-2/cmd/service/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
+	"os"
 )
 
-func getKel(arr []int, k int) int {
+func getKel(arr []int, k int) (int, error) {
+    if (k > len(arr)) {
+        return 0, errors.New("k больше размера массива")
+    }
 	var tmp []int = []int{}
 	mn := math.MaxInt
 	for i := 0; i < k; i++ {
@@ -26,10 +31,39 @@ func getKel(arr []int, k int) int {
 			mn = temp
 		}
 	}
-	return mn
+	return mn, nil
 }
 
 func main() {
-    var arr []int = []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
-	fmt.Println(getKel(arr, 4))
+    fmt.Println("Введите количество элементов массива:")
+    var size int
+    _, err := fmt.Scan(&size)
+    if (err != nil) {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    fmt.Println("Введите массив:")
+    var arr []int
+    for i := 0; i < size; i++ {
+        var tmp int
+        _, err = fmt.Scan(&tmp)
+        if (err != nil) {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+        arr = append(arr, tmp)
+    }
+    fmt.Println("Введите k:")
+    var k int
+    _, err = fmt.Scan(&k)
+    if (err != nil) {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    res, err := getKel(arr, k)
+    if (err != nil) {
+        fmt.Println(err)
+        os.Exit(2)
+    }
+    fmt.Println(res)
 }
