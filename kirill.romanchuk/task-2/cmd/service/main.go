3d6d9a34e7315af287733c3926a9c7513b1cb046
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-10-03 14:59:01 +0300

    add reader

--- kirill.romanchuk/task-2/cmd/service/main.go
+++ kirill.romanchuk/task-2/cmd/service/main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"fmt"
+)
+
+func reader() ([]float64, int) {
+	var nums []float64
+	var k int
+	fmt.Println("Вводите числа для заполнения массива\n" +
+		"(для завершения ввода нажмите Ctrl + Z (Windows) или Ctrl + D (Unix)): ")
+
+	for {
+		var num float64
+		_, err := fmt.Scan(&num)
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			fmt.Println("Некорректный ввод. Пожалуйста попробуйте ещё раз")
+			var dummy string
+			fmt.Scanln(&dummy)
+		}
+		nums = append(nums, num)
+	}
+
+	fmt.Println("Введите целочисленное значение k, чтобы получить k-й наибольший элемент массива:")
+
+	for {
+		_, err := fmt.Scan(&k)
+		if err == nil {
+			break
+		}
+		fmt.Println("Некорректный ввод. Пожалуйста попробуйте ещё раз")
+		var dummy string
+		fmt.Scanln(&dummy)
+	}
+	return nums, k
+}
+
 func main() {
+	nums, k := reader()
+	fmt.Print(nums, k)
 }
