7a75b49eed3a5dfdf443c16bae998aaf961212ba
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2024-10-04 22:45:14 +0300

    [TASK-2] Add implementation of findKElement

--- danil.novokhatskiy/task-2/cmd/service/main.go
+++ danil.novokhatskiy/task-2/cmd/service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"log"
 	"os"
@@ -9,6 +10,37 @@ import (
 	"strings"
 )
 
+type MiniHeap []int
+
+func (h MiniHeap) Len() int { return len(h) }
+
+func (h MiniHeap) Less(i, j int) bool { return h[i] < h[j] }
+
+func (h MiniHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *MiniHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MiniHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+func findKElement(arr []int, k int) int {
+	h := &MiniHeap{}
+	heap.Init(h)
+	for _, num := range arr {
+		heap.Push(h, num)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	return heap.Pop(h).(int)
+}
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Введите числа, разделенные пробелами:")
@@ -17,10 +49,10 @@ func main() {
 
 	numbers := strings.Split(input, " ")
 
-	var numbersArray []int
+	var arr []int
 	for _, numberStr := range numbers {
 		number, _ := strconv.Atoi(numberStr)
-		numbersArray = append(numbersArray, number)
+		arr = append(arr, number)
 	}
 	fmt.Println("Введите k: ")
 	fmt.Print("> ")
@@ -30,5 +62,10 @@ func main() {
 		log.Fatal(err)
 		return
 	}
-	fmt.Println("Считанные числа:", numbersArray)
+	if k > 0 && k <= len(arr) {
+		result := findKElement(arr, k)
+		fmt.Printf("%d-й наибольший элемент массива: %d\n", k, result)
+	} else {
+		fmt.Println("Некорректное значение k")
+	}
 }
