5cff2d359e82becd7b23c61e3e2772b70802ad83
Dudkina Sofia <94846666+sssidkn@users.noreply.github.com>
2024-10-07 19:55:43 +0300

    [TASK-2-1] add errors

--- sofia.dudkina/task-2-1/cmd/service/main.go
+++ sofia.dudkina/task-2-1/cmd/service/main.go
@@ -1,14 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	var N, K int
 	var minT, maxT, T int
 	var op string
-	fmt.Scan(&N)
+	_, err := fmt.Scan(&N)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	for ; N > 0; N-- {
-		fmt.Scan(&K)
+		_, err = fmt.Scan(&K)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		minT = 15
 		maxT = 30
 		for ; K > 0; K-- {
