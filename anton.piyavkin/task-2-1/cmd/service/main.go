d722dff7d287a193785197631f2f9f965838b4c8
Piyavva <piyavkin05@bk.ru>
2024-10-07 13:16:03 +0300

    [TASK-2-2] Add solution

--- anton.piyavkin/task-2-1/cmd/service/main.go
+++ anton.piyavkin/task-2-1/cmd/service/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	var n int
+	_, err := fmt.Scan(&n)
+    if err != nil {
+        fmt.Fprint(os.Stderr, err)
+        os.Exit(1)
+    }
+    for i := 0; i < n; i++ {
+        lowerBound := 15
+        upperBound := 30
+        var k int
+        _, err = fmt.Scan(&k)
+        if err != nil {
+            fmt.Fprint(os.Stderr, err)
+            os.Exit(1)
+        }
+        for j := 0; j < k; j++ {
+            var str string
+            _, err = fmt.Scan(&str)
+            if err != nil {
+                fmt.Fprint(os.Stderr, err)
+                os.Exit(1)
+            }
+            if (str[0] != '<' && str[0] != '>' && str[1] != '=') || len(str) != 2 {
+                fmt.Fprint(os.Stderr, "invalid input\n")
+                os.Exit(2)
+            }
+            var temp int
+            _, err = fmt.Scan(&temp)
+            if err != nil || (temp > 30 || temp < 15) {
+                fmt.Fprint(os.Stderr, "invalid input\n")
+                os.Exit(3)
+            }
+            if str[0] == '<' {
+                upperBound = min(upperBound, temp)
+            } else {
+                lowerBound = max(lowerBound, temp)
+            }
+            if (lowerBound < upperBound) {
+                fmt.Println(upperBound)
+            } else {
+                fmt.Println(-1)
+            }
+        }
+    }
 }
