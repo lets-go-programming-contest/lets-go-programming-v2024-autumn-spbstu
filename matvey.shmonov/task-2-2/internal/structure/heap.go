2c363ad137dff8fc0fddf700a9ea886f1d0aae66
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-10-08 15:27:07 +0300

    [TASK-2-2] Initial commit

---
+++ matvey.shmonov/task-2-2/internal/structure/heap.go
@@ -0,0 +1,28 @@
+// Implementation of heap.Interface and sort.Interface
+package structure
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool { // negative
+	return (*h)[i] > (*h)[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
