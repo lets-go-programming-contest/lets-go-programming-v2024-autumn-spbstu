0030e2c8b9210257506abff6360dfca76e1ca2db
Mark Zheleznyakov <mrqiz@internet.ru>
2024-10-08 22:11:11 +0300

    [TASK-2-1] Added climate calculation

--- mark.zheleznyakov/task-2-1/cmd/service/main.go
+++ mark.zheleznyakov/task-2-1/cmd/service/main.go
@@ -14,11 +14,24 @@ func main() {
 	depts := input.ReadInt("departments count")
 
 	for _ = range depts {
+		minTemp := initialMinTemp
+		maxTemp := initialMaxTemp
 		workersPerDept := input.ReadInt("workers per department")
     
     for _ = range workersPerDept {
       condition, tVal := input.ReadCondition()
-			fmt.Println(condition, tVal)
+      
+			if condition == ">=" && tVal > minTemp {
+				minTemp = tVal
+			} else if condition == "<=" && tVal < maxTemp {
+				maxTemp = tVal
+			}
+		}
+
+    if minTemp > maxTemp {
+			fmt.Println(-1)
+		} else {
+			fmt.Println(minTemp)
 		}
 	}
 }
