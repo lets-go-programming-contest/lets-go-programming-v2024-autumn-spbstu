0d614134b56b291e5f2c8c3c62af0b439752e2c6
nutochk <kozakova.ann@mail.ru>
2024-10-08 21:00:23 +0300

    create a project structure'

--- ann.kozakova/task-2-2/cmd/service/main.go
+++ ann.kozakova/task-2-2/cmd/service/main.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-	"container/heap"
 	"errors"
 	"fmt"
 	"log"
+
+	"github.com/nutochk/task-2-2/internal/max"
 )
-
-type IntHeap []int
-
-func (h IntHeap) Len() int {
-	return len(h)
-}
-
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] < h[j]
-}
-
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
-
-func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
 
 func main() {
 	fmt.Println("Enter n:")
@@ -61,25 +36,9 @@ func main() {
 		log.Fatal(errors.New("incorrect data"))
 	}
 
-	kMax, err := findKMax(&numbers, k)
+	kMax, err := max.FindKMax(&numbers, k)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(kMax)
-}
-
-func findKMax(nums *[]int, k int) (int, error) {
-	if k <= 1 || k > len((*nums)) {
-		return 0, errors.New("incorrect k")
-	}
-
-	h := &IntHeap{}
-	heap.Init(h)
-	for _, num := range *nums {
-		heap.Push(h, num)
-		if h.Len() > k {
-			heap.Pop(h)
-		}
-	}
-	return (*h)[0], nil
 }
