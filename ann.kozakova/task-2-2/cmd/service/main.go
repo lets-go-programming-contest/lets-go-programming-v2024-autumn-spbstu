e1ce60eff9633e7efc271b475babbc40378ff4d3
nutochk <kozakova.ann@mail.ru>
2024-10-10 21:10:52 +0300

    separate input

--- ann.kozakova/task-2-2/cmd/service/main.go
+++ ann.kozakova/task-2-2/cmd/service/main.go
@@ -1,40 +1,16 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 
+	"github.com/nutochk/task-2-2/internal/data"
 	"github.com/nutochk/task-2-2/internal/max"
 )
 
 func main() {
-	fmt.Println("Enter n:")
-	var n int
-	_, err := fmt.Scan(&n)
-	if err != nil {
-		log.Fatal(errors.New("incorrect data"))
-	}
 
-	fmt.Println("Enter ai:")
-
-	numbers := make([]int, n)
-	i := 0
-	var ai int
-	for ; i < n; i++ {
-		_, err = fmt.Scan(&ai)
-		if err != nil {
-			log.Fatal(errors.New("incorrect data"))
-		}
-		numbers[i] = ai
-	}
-
-	fmt.Println("Enter k:")
-	var k int
-	_, err = fmt.Scan(&k)
-	if err != nil {
-		log.Fatal(errors.New("incorrect data"))
-	}
+	numbers, k := data.EnterData()
 
 	kMax, err := max.FindKMax(&numbers, k)
 	if err != nil {
