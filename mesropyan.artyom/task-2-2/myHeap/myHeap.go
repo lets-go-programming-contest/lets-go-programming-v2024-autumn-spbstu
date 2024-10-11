de8eca091b299e5533802cde434b0ada1e5258e2
artem6554 <artem6554@gmail.com>
2024-10-11 22:55:51 +0300

    [TASK-2-2] made heap receivers pointers

--- mesropyan.artyom/task-2-2/myHeap/myHeap.go
+++ mesropyan.artyom/task-2-2/myHeap/myHeap.go
@@ -1,34 +1,32 @@
 package myHeap
 
-type MyHeap struct {
-	Elems []int
-}
+type MyHeap []int
 
-func (h MyHeap) Push(x any) {
+func (h *MyHeap) Push(x interface{}) {
 	if elem, ok := x.(int); ok {
-		h.Elems = append(h.Elems, elem)
+		*h = append(*h, elem)
 	}
 }
 
-func (h MyHeap) Pop() any {
+func (h *MyHeap) Pop() interface{} {
 	if h.Len() != 0 {
-		result := h.Elems[h.Len()-1]
-		h.Elems = h.Elems[:h.Len()-1]
+		result := (*h)[(h).Len()-1]
+		*h = (*h)[:h.Len()-1]
 		return result
 	}
 	return 0
 }
 
-func (h MyHeap) Len() int {
-	return len(h.Elems)
+func (h *MyHeap) Len() int {
+	return len(*h)
 }
 
-func (h MyHeap) Less(i, j int) bool {
-	return h.Elems[i] < h.Elems[j]
+func (h *MyHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
 }
 
-func (h MyHeap) Swap(i, j int) {
-	temp := h.Elems[i]
-	h.Elems[i] = h.Elems[j]
-	h.Elems[j] = temp
+func (h *MyHeap) Swap(i, j int) {
+	temp := (*h)[i]
+	(*h)[i] = (*h)[j]
+	(*h)[j] = temp
 }
