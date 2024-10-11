67e9d18a02aff385290f2e1d94b55a59a4d85ad4
Kirill <kirillromancuk72@gmail.com>
2024-10-11 23:59:26 +0300

    [TASK-2-1] add readNum func

--- kirill.romanchuk/task-2-1/cmd/service/main.go
+++ kirill.romanchuk/task-2-1/cmd/service/main.go
@@ -1,7 +1,29 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"os"
+)
+
+func readNum(message string, min int, max int) int {
+	var num int
+	fmt.Print(message)
+	_, err := fmt.Scan(&num)
+	if err != nil {
+		panic("ошибка: некорректное значение")
+	}
+	if num < min || num > max {
+		panic(fmt.Sprintf("ошибка: значение должно быть в диапазоне от %d до %d", min, max))
+	}
+	return num
+}
 
 func main() {
-
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+			os.Exit(1)
+		}
+	}()
+	fmt.Print(readNum("test: ", 1, 2000))
 }
