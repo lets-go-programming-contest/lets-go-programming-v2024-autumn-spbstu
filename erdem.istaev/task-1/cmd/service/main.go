28d60a01a9447339025ea7e54aea60d3a2b5674f
istaeverd3@gmail.com <istaeverd3@gmail.com>
2024-10-11 15:04:45 +0300

    [TASK-1] Moved reading functions from main to another package

--- erdem.istaev/task-1/cmd/service/main.go
+++ erdem.istaev/task-1/cmd/service/main.go
@@ -2,36 +2,31 @@ package main
 
 import (
 	"bufio"
-	"erdem.istaev/task-1/internal/calculator"
-	"errors"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
-)
 
-var (
-	ErrIncorrectNumber = errors.New("Некорректное число. Пожалуйста, введите числовое значение.")
+	"erdem.istaev/task-1/internal/calculator"
+	"erdem.istaev/task-1/internal/input"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Введите первое число: ")
-	op1, err := readFloat64(reader)
+	op1, err := input.ReadFloat64(reader)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Print("Выберите операцию (+, -, *, /): ")
-	operation, err := readOperation(reader)
+	operation, err := input.ReadOperation(reader)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Print("Введите второе число: ")
-	op2, err := readFloat64(reader)
+	op2, err := input.ReadFloat64(reader)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -44,29 +39,4 @@ func main() {
 	}
 
 	fmt.Printf("Результат: %g %s %g = %g", op1, operation, op2, res)
-}
-
-func readFloat64(reader *bufio.Reader) (float64, error) {
-	str, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-	remSpc := strings.TrimSpace(str)
-	var res float64
-	if res, err = strconv.ParseFloat(remSpc, 64); err != nil {
-		return 0, ErrIncorrectNumber
-	}
-	return res, nil
-}
-
-func readOperation(reader *bufio.Reader) (string, error) {
-	str, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err
-	}
-	operation := strings.TrimSpace(str)
-	if operation != "+" && operation != "-" && operation != "*" && operation != "/" {
-		return "", calculator.ErrIncorrectOperation
-	}
-	return operation, nil
 }
