c7e48f8335925ad0e98d7f5ba228091b59a6b5a1
artem6554 <artem6554@gmail.com>
2024-10-11 17:42:11 +0300

    [TASK-2-2] moved OptimalTemp to other package

--- mesropyan.artyom/task-2-1/cmd/task-2-1/task-2-1.go
+++ mesropyan.artyom/task-2-1/cmd/task-2-1/task-2-1.go
@@ -1,45 +1,9 @@
 package main
 
-import "fmt"
+import (
+	optimaltemp "github.com/artem6554/task-2-1/optimalTemp"
+)
 
 func main() {
-	var depCount int
-	var emplCount int
-	var op string
-	var temp int
-	moreThan := -1
-	lessThan := 31
-
-	fmt.Scan(&depCount)
-	for i := 0; i < depCount; i++ {
-		fmt.Scan(&emplCount)
-		for j := 0; j < emplCount; j++ {
-			fmt.Scan(&op)
-			fmt.Scan(&temp)
-			switch op {
-			case ">=":
-				if temp > moreThan {
-					moreThan = temp
-				}
-			case "<=":
-				if temp < lessThan {
-					lessThan = temp
-				}
-			default:
-				fmt.Println("Incorrect input!")
-			}
-			switch {
-			case lessThan == 31:
-				fmt.Println(moreThan)
-			case moreThan == -1:
-				fmt.Println(lessThan)
-			case moreThan <= lessThan:
-				fmt.Println(moreThan)
-			default:
-				fmt.Println(-1)
-			}
-		}
-		moreThan = -1
-		lessThan = 31
-	}
+	optimaltemp.OptimalTemp()
 }
