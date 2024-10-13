9682afdd7877a33c0d76021747e4a9a55b50825b
istaeverd3@gmail.com <istaeverd3@gmail.com>
2024-10-13 20:09:18 +0300

    [TASK-2-2] Split the main function

--- erdem.istaev/task-2-1/internal/input/read_symbols.go
+++ erdem.istaev/task-2-1/internal/input/read_symbols.go
@@ -2,15 +2,12 @@ package input
 
 import (
 	"bufio"
-	"errors"
+	"fmt"
 	"strconv"
 	"strings"
-)
 
-var (
-	ErrIncorrectNumber       = errors.New("Некорректное число. Пожалуйста, введите числовое значение.")
-	ErrIncorrectSeparator    = errors.New("Строка должна содержать ровно один пробел для разделения.")
-	ErrIncorrectComparsionOp = errors.New("Некорректная операция сравнения. Пожалуйста, введите \">=\" или \"<=\".")
+	"erdem.istaev/task-2-1/internal/errors"
+	"erdem.istaev/task-2-1/internal/structure"
 )
 
 func ReadInt(reader *bufio.Reader) (int, error) {
@@ -21,7 +18,7 @@ func ReadInt(reader *bufio.Reader) (int, error) {
 	remSpc := strings.TrimSpace(str)
 	var res int
 	if res, err = strconv.Atoi(remSpc); err != nil {
-		return 0, ErrIncorrectNumber
+		return 0, errors.ErrIncorrectNumber
 	}
 	return res, nil
 }
@@ -34,17 +31,46 @@ func ReadCondition(reader *bufio.Reader) (string, int, error) {
 	remSpc := strings.TrimSpace(str)
 	parts := strings.Split(remSpc, " ")
 	if len(parts) != 2 {
-		return "", 0, ErrIncorrectSeparator
+		return "", 0, errors.ErrIncorrectSeparator
 	}
 
 	if parts[0] != ">=" && parts[0] != "<=" {
-		return "", 0, ErrIncorrectComparsionOp
+		return "", 0, errors.ErrIncorrectComparsionOp
 	}
 
 	var temp int
 	if temp, err = strconv.Atoi(parts[1]); err != nil {
-		return "", 0, ErrIncorrectNumber
+		return "", 0, errors.ErrIncorrectNumber
 	}
 
 	return parts[0], temp, nil
+}
+
+func SetTemperature(reader *bufio.Reader, n int, bounds []structure.TemperatureBounds) {
+	var condition string
+	var temp int
+	for i := 0; i < n; i++ {
+		fmt.Print("Введите количество сотрудников: ")
+		k, err := ReadInt(reader)
+		if err != nil {
+			fmt.Println(errors.ErrIncorrectNumber)
+		}
+
+		for j := 0; j < k; j++ {
+			fmt.Print("Укажите температуру: ")
+			condition, temp, err = ReadCondition(reader)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			bounds[i].UpdateBounds(condition, temp)
+			if bounds[i].IsValid() {
+				fmt.Println(bounds[i].Lower)
+			} else {
+				fmt.Println(-1)
+				return
+			}
+		}
+	}
 }
