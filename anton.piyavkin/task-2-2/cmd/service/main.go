7adc97183def8ba7ba5b4a5c3096b33aa71b247a
Piyavva <piyavkin05@bk.ru>
2024-10-14 18:53:40 +0300

    [TASK-2-2] Split into files

--- anton.piyavkin/task-2-2/cmd/service/main.go
+++ anton.piyavkin/task-2-2/cmd/service/main.go
@@ -1,51 +1,11 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
 	"os"
+
+	"github.com/Piyavva/task-2-2/internal/getDish"
 )
-
-type Heap []int
-
-func (h Heap) Len() int {
-	return len(h)
-}
-
-func (h Heap) Less(i, j int) bool {
-	return h[i] < h[j]
-}
-
-func (h Heap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
-
-func (h *Heap) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-
-func (h *Heap) Pop() interface{} {
-	size := len(*h)
-	x := (*h)[size - 1]
-    (*h)[size - 1] = 0
-	*h = (*h)[:size - 1]
-	return x
-}
-
-func getKDish(dishes []int, k int) int {
-    h := &Heap{}
-	heap.Init(h)
-    for i := 0; i < k; i++ {
-        heap.Push(h, dishes[i])
-    }
-    for i := k; i < len(dishes); i++ {
-        if dishes[i] > (*h)[0] {
-            heap.Pop(h)
-            heap.Push(h, dishes[i])
-        }
-    }
-    return (*h)[0]
-}
 
 func main() {
 	var n int
@@ -70,6 +30,6 @@ func main() {
         fmt.Fprint(os.Stderr, "invalid k\n")
         os.Exit(1)
     }
-    res := getKDish(dishes, k)
+    res := getDish.GetKDish(dishes, k)
 	fmt.Println(res)
 }
--- anton.piyavkin/task-2/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Piyavva/task-2
-
-go 1.23.1
