e4e4855a1657cc78288e96fff9fc8026159b93ae
Piyavva <piyavkin05@bk.ru>
2024-10-14 17:27:31 +0300

    [TASK-2-1] Break the main into functions

--- anton.piyavkin/task-2-1/cmd/service/main.go
+++ anton.piyavkin/task-2-1/cmd/service/main.go
@@ -1,53 +1,85 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	var n int
-	_, err := fmt.Scan(&n)
+func readInt(in io.Reader) (int, error) {
+    reader := bufio.NewReader(in)
+    input, err := reader.ReadString('\n')
     if err != nil {
-        fmt.Fprint(os.Stderr, err)
-        os.Exit(1)
+        return 0, err
     }
+    input = strings.TrimSpace(input)
+    value, err := strconv.Atoi(input)
+    if err != nil {
+        return 0, err
+    }
+    return value, nil
+}
+
+func writeInt(value int, out io.Writer) {
+    writer := bufio.NewWriter(out)
+    defer writer.Flush()
+    fVal := strconv.Itoa(value)
+    writer.WriteString(fVal)
+    writer.WriteByte('\n')
+}
+
+func getTemp(in io.Reader, out io.Writer, n int) error {
     for i := 0; i < n; i++ {
         lowerBound := 15
         upperBound := 30
-        var k int
-        _, err = fmt.Scan(&k)
+        k, err := readInt(in)
         if err != nil {
-            fmt.Fprint(os.Stderr, err)
-            os.Exit(1)
+            return err
         }
         for j := 0; j < k; j++ {
             var str string
-            _, err = fmt.Scan(&str)
+            reader := bufio.NewReader(in)
+            str, err = reader.ReadString('\n')
             if err != nil {
-                fmt.Fprint(os.Stderr, err)
-                os.Exit(1)
+                return err
             }
-            if (str[0] != '<' && str[0] != '>' && str[1] != '=') || len(str) != 2 {
-                fmt.Fprint(os.Stderr, "invalid input\n")
-                os.Exit(2)
+            str = strings.TrimSpace(str)
+            if (str[0] != '<' && str[0] != '>' && str[1] != '=') {
+                return errors.New("invalid input")
             }
-            var temp int
-            _, err = fmt.Scan(&temp)
+            num := strings.Split(str, " ")
+            temp, err := strconv.Atoi(num[1])
             if err != nil || (temp > 30 || temp < 15) {
-                fmt.Fprint(os.Stderr, "invalid input\n")
-                os.Exit(3)
+                return errors.New("invalid input2")
             }
             if str[0] == '<' {
                 upperBound = min(upperBound, temp)
             } else {
                 lowerBound = max(lowerBound, temp)
             }
-            if (lowerBound < upperBound) {
-                fmt.Println(upperBound)
+            if (lowerBound <= upperBound) {
+                writeInt(upperBound, out)
             } else {
-                fmt.Println(-1)
+                writeInt(-1, out)
             }
         }
+    }
+    return nil
+}
+
+func main() {
+	n, err := readInt(os.Stdin)
+    if err != nil {
+        fmt.Fprint(os.Stderr, err)
+        os.Exit(1)
+    }
+    err = getTemp(os.Stdin, os.Stdout, n)
+    if err != nil {
+        fmt.Fprint(os.Stderr, err)
+        os.Exit(1)
     }
 }
