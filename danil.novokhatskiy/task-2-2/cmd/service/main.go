0369ef72f7c24902915b317b01d91ffb06d622d7
katagiri <karuk.vova2005@mail.ru>
2024-10-14 21:25:13 +0300

    [TASK-2-2] Add heap.go and delete extra logs

--- danil.novokhatskiy/task-2-2/cmd/service/main.go
+++ danil.novokhatskiy/task-2-2/cmd/service/main.go
@@ -8,31 +8,11 @@ import (
 	"github.com/katagiriwhy/task-2-2/internal"
 )
 
-type Heap []int
-
-func (h Heap) Len() int { return len(h) }
-
-func (h Heap) Less(i, j int) bool { return h[i] < h[j] }
-
-func (h Heap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-
-func (h *Heap) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-
-func (h *Heap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
 func findKDish(arr *[]int, k int) {
 	if k > len(*arr) || k <= 1 {
 		log.Fatal("Invalid input")
 	}
-	h := &Heap{}
+	h := &internal.Heap{}
 	heap.Init(h)
 	for _, v := range *arr {
 		heap.Push(h, v)
--- danil.novokhatskiy/task-2-2/internal/readData.go
+++ danil.novokhatskiy/task-2-2/internal/readData.go
@@ -2,16 +2,12 @@ package internal
 
 import (
 	"fmt"
-	"log"
 )
 
 func ReadData() ([]int, int, error) {
 	fmt.Print("Enter the number of dishes: ")
 	var num int
 	_, err := fmt.Scan(&num)
-	if err != nil {
-		log.Fatal(err)
-	}
 	arr := make([]int, num)
 	for i := 0; i < num; i++ {
 		var tmp int
@@ -24,8 +20,5 @@ func ReadData() ([]int, int, error) {
 	fmt.Println("Enter k value: ")
 	var k int
 	_, err = fmt.Scan(&k)
-	if err != nil {
-		log.Fatal(err)
-	}
 	return arr, k, err
 }
