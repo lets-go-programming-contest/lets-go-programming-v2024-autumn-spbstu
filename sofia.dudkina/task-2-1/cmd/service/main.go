33eb0e9ad897d06eace061c4d20e67841b18331a
Sonya <sonyadudkina0307@gmail.com>
2024-10-14 16:17:24 +0300

    [TASK-2-1] packages added

--- sofia.dudkina/task-2-1/cmd/service/main.go
+++ sofia.dudkina/task-2-1/cmd/service/main.go
@@ -1,31 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sssidkn/task-2-1/internal/optimalt"
+)
 
 func main() {
-	var N, K int
-	var minT, maxT, T int
-	var op string
-	fmt.Scan(&N)
-	for ; N > 0; N-- {
-		fmt.Scan(&K)
-		minT = 15
-		maxT = 30
-		for ; K > 0; K-- {
-			fmt.Scan(&op, &T)
-			switch op {
-			case ">=":
-				if T >= 15 {
-					minT = max(T, minT)
-				}
-			case "<=":
-				maxT = min(T, maxT)
-			}
-			if minT > maxT {
-				fmt.Println(-1)
-			} else {
-				println(minT)
-			}
+	var N int
+	_, err := fmt.Scan(&N)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		err = optimalt.Find(N)
+		if err != nil {
+			fmt.Println(err)
 		}
 	}
 }
