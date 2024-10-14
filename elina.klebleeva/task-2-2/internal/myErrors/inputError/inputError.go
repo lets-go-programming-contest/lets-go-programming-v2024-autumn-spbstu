25a1084a6ceb078e9e153c3e260664683e7769e6
EmptyInsid <elinaklebleeva001@gmail.com>
2024-10-14 20:15:10 +0300

    [TASK-2-2] change pointer in myError

--- elina.klebleeva/task-2-2/internal/myErrors/inputError/inputError.go
+++ elina.klebleeva/task-2-2/internal/myErrors/inputError/inputError.go
@@ -6,6 +6,6 @@ type IncorrectNumber struct {
 	ErrorPlace string
 }
 
-func (err *IncorrectNumber) Error() string {
+func (err IncorrectNumber) Error() string {
 	return fmt.Sprintf("incorrect numder input in: %s", err.ErrorPlace)
 }
