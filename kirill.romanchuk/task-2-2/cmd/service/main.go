4f9f19ce6f95c5a9ef03713e04dabf253c11679c
Kirill <kirillromancuk72@gmail.com>
2024-10-14 16:41:19 +0300

    [TASK-2-2] add readers

--- kirill.romanchuk/task-2-2/cmd/service/main.go
+++ kirill.romanchuk/task-2-2/cmd/service/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/kirill.romanchuk/task-2-2/internal/utils"
+)
+
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+			os.Exit(1)
+		}
+	}()
+
+	n, values, k := utils.ReadInput()
+	fmt.Print(n, values, k)
 }
