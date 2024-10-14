3b50e80ea3ad0eb018892825fd48e046ca550057
nutochk <kozakova.ann@mail.ru>
2024-10-14 20:05:51 +0300

    [TASK-2-2] int heap fix

--- ann.kozakova/task-2-2/pkg/int_heap/int_heap.go
+++ ann.kozakova/task-2-2/pkg/int_heap/int_heap.go
@@ -10,21 +10,21 @@ func (h IntHeap) Less(i, j int) bool {
 	return h[i] < h[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
-func (h IntHeap) Push(x interface{}) {
+func (h *IntHeap) Push(x interface{}) {
 	a, ok := x.(int)
 	if ok {
-		h = append(h, a)
+		*h = append(*h, a)
 	}
 }
 
-func (h IntHeap) Pop() interface{} {
-	old := h
+func (h *IntHeap) Pop() interface{} {
+	old := *h
 	n := len(old)
 	x := old[n-1]
-	h = old[0 : n-1]
+	*h = old[0 : n-1]
 	return x
 }
