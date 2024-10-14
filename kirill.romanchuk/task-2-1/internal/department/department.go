084dccb68cd61b0428212ebfe2a09876db5e11f0
Kirill <kirillromancuk72@gmail.com>
2024-10-14 19:55:05 +0300

    [TASK-2-1] add UpdateBounds func

--- kirill.romanchuk/task-2-1/internal/department/department.go
+++ kirill.romanchuk/task-2-1/internal/department/department.go
@@ -16,18 +16,22 @@ func NewDepartment() Department {
 	return Department{lowerBound: utils.LowerBound, upperBound: utils.UpperBound}
 }
 
+func (d *Department) UpdateBounds(condition string, temperature int) {
+	switch condition {
+	case ">=":
+		d.lowerBound = int(math.Max(float64(d.lowerBound), float64(temperature)))
+	case "<=":
+		d.upperBound = int(math.Min(float64(d.upperBound), float64(temperature)))
+	}
+}
+
 func (d *Department) ManageTemperature() {
 	k := utils.ReadIntNum("Введите количество сотрудников (1-2000): ", 1, 2000)
 
 	for j := 0; j < k; j++ {
 		condition, temperature := utils.ReadConditionAndTemperature()
 
-		switch condition {
-		case ">=":
-			d.lowerBound = int(math.Max(float64(d.lowerBound), float64(temperature)))
-		case "<=":
-			d.upperBound = int(math.Min(float64(d.upperBound), float64(temperature)))
-		}
+		d.UpdateBounds(condition, temperature)
 
 		if d.lowerBound > d.upperBound {
 			fmt.Println("Невозможно подобрать температуру для этого отдела\n", -1)
