2dc5e3053b15d6a3f3e9e79fd74dee0525f15830
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-10-19 23:21:27 +0300

    [TASK-2-2] Control of inputs & result added to main

--- andrey.kostin/task-2-2/cmd/service/main.go
+++ andrey.kostin/task-2-2/cmd/service/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/IDevFrye/task-2-2/internal/findDish"
+	reader "github.com/IDevFrye/task-2-2/internal/io"
+)
+
+var (
+	minN_k = 1
+	maxN   = 10000
+	minAi  = -10000
+	maxAi  = 10000
+)
+
+func main() {
+	countOfDishes, err := reader.GetNumber("Введите количество блюд на шведском столе: ", minN_k, maxN)
+	if err != nil {
+		log.Fatal(err)
+	}
+	preferences, err := reader.GetHeapElements("Введите предпочтение к блюду: ", minAi, maxAi, countOfDishes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	numberOfPreference, err := reader.GetPrefDish("Введите порядковый номер блюда по предпочтению: ", minN_k, countOfDishes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := findDish.FindDish(&preferences, numberOfPreference)
+	fmt.Println("Число, соответствующее", numberOfPreference, "- му наиболее предпочтительному блюду:", result)
 }
--- andrey.kostin/task-2-2/internal/io/io.go
+++ andrey.kostin/task-2-2/internal/io/io.go
@@ -9,12 +9,12 @@ import (
 	errProc "github.com/IDevFrye/task-2-2/internal/errors"
 )
 
-func getNumber(prompt string, minVal int, maxVal int) (int, error) {
+func GetNumber(prompt string, minVal int, maxVal int) (int, error) {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print(prompt)
 	scanner.Scan()
 	input := scanner.Text()
-	if numb, err := strconv.Atoi(input); err != nil {
+	if numb, err := strconv.Atoi(input); err == nil {
 		if numb <= maxVal && numb >= minVal {
 			return numb, nil
 		} else {
@@ -25,14 +25,14 @@ func getNumber(prompt string, minVal int, maxVal int) (int, error) {
 	}
 }
 
-func getHeapElement(prompt string, minVal int, maxVal int, count int) ([]int, error) {
+func GetHeapElements(prompt string, minVal int, maxVal int, count int) ([]int, error) {
 	scanner := bufio.NewScanner(os.Stdin)
 	prefs := make([]int, count)
 	for i := 0; i < count; i++ {
 		fmt.Print(prompt)
 		scanner.Scan()
 		input_i := scanner.Text()
-		if numb_i, err := strconv.Atoi(input_i); err != nil {
+		if numb_i, err := strconv.Atoi(input_i); err == nil {
 			if numb_i <= maxVal && numb_i >= minVal {
 				prefs[i] = numb_i
 			} else {
@@ -45,10 +45,12 @@ func getHeapElement(prompt string, minVal int, maxVal int, count int) ([]int, er
 	return prefs, nil
 }
 
-func getPrefDish(prompt string, minVal int, count int) (int, error) {
+func GetPrefDish(prompt string, minVal int, count int) (int, error) {
 	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(prompt)
+	scanner.Scan()
 	input := scanner.Text()
-	if numb, err := strconv.Atoi(input); err != nil {
+	if numb, err := strconv.Atoi(input); err == nil {
 		if numb <= count && numb >= minVal {
 			return numb, nil
 		} else {
