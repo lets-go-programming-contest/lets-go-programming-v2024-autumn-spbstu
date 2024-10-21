acf2da8896fb00d76f80999ae015f9a9beba4aa9
Удалов Александр <udalov2003@rambler.ru>
2024-10-21 14:16:27 +0300

    TASK-2-2

--- alex.udalov/task-2-2/cmd/service/main.go
+++ alex.udalov/task-2-2/cmd/service/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"container/heap"
+	"fmt"
+	"task-2-2/internal/meatHeap"
+	"task-2-2/internal/reader"
+)
+
+func main() {
+	dishes := &meatHeap.MeatHeap{}
+	heap.Init(dishes)
+	n := reader.ReadNumber()
+	reader.ReadToHeap(dishes, n)
+	var k int
+	for {
+		k = reader.ReadNumber()
+		if k > n {
+			fmt.Println("k cannot be bigger than n")
+			continue
+		}
+		break
+	}
+	dish := meatHeap.FindDish(dishes, k)
+	fmt.Println(dish)
+}
--- alex.udalov/task-2-2/internal/reader/reader.go
+++ alex.udalov/task-2-2/internal/reader/reader.go
@@ -1,1 +1,25 @@
 package reader
+
+import (
+	"container/heap"
+	"fmt"
+	"os"
+	"task-2-2/internal/meatHeap"
+)
+
+func ReadNumber() int {
+	var n int
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		fmt.Println("unable to read a number")
+		os.Exit(1)
+	}
+	return n
+}
+
+func ReadToHeap(h *meatHeap.MeatHeap, n int) {
+	for range n {
+		currentDish := ReadNumber()
+		heap.Push(h, currentDish)
+	}
+}
