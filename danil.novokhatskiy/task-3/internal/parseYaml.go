ae18baf2f4afec7816f50f01c7dfb3fbacd082d5
katagiri <karuk.vova2005@mail.ru>
2024-10-25 10:00:19 +0300

    [TASK-3] Add config structure and parse flag

---
+++ danil.novokhatskiy/task-3/internal/parseYaml.go
@@ -0,0 +1,32 @@
+package internal
+
+import (
+	"flag"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+func ReadFlag() string {
+	loc := ""
+	flag.StringVar(&loc, "cfg", "", "Config to read")
+	flag.Parse()
+	return loc
+}
+
+type Config struct {
+	InputFile string `yaml:"input_file"`
+	OutputDir string `yaml:"output_file"`
+}
+
+func ReadAndParseConfig(cfg *Config, f string) error {
+	file, err := os.ReadFile(f)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(file, cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
