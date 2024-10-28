a033ebedbaa23614fb740a423f62827139b77f61
Piyavva <piyavkin05@bk.ru>
2024-10-28 18:04:00 +0300

    [TASK-3] Add output

--- anton.piyavkin/task-3/cmd/service/main.go
+++ anton.piyavkin/task-3/cmd/service/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Piyavva/task-3/internal/input"
+	"github.com/Piyavva/task-3/internal/output"
 	"github.com/Piyavva/task-3/internal/parse"
 )
 
@@ -10,5 +10,5 @@ func main() {
 	cfg := parse.ParseConfig()
 	valQuotes := input.ReadFile(cfg)
 	input.SortCurrencies(&valQuotes)
-	fmt.Println(valQuotes)
+	output.Write(valQuotes, cfg)
 }
