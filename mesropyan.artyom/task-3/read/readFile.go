0a8348491af41d5fca8bf8e4728ed57906b389a9
artem6554 <artem6554@gmail.com>
2024-10-30 23:39:57 +0300

    [TASK-3] changed xml reader to work with windows-1251 encoding

--- mesropyan.artyom/task-3/read/readFile.go
+++ mesropyan.artyom/task-3/read/readFile.go
@@ -1,11 +1,13 @@
 package read
 
 import (
+	"bytes"
 	"encoding/xml"
 	"os"
 	"strings"
 
 	"github.com/artem6554/task-3/structs"
+	"golang.org/x/net/html/charset"
 )
 
 func ParseXML(inputFile string) structs.ValCurs {
@@ -16,7 +18,9 @@ func ParseXML(inputFile string) structs.ValCurs {
 	data = []byte(strings.ReplaceAll(string(data), ",", "."))
 
 	var currencies structs.ValCurs
-	err = xml.Unmarshal(data, &currencies)
+	decoder := xml.NewDecoder(bytes.NewReader(data))
+	decoder.CharsetReader = charset.NewReaderLabel
+	err = decoder.Decode(&currencies)
 	if err != nil {
 		panic(err)
 	}
