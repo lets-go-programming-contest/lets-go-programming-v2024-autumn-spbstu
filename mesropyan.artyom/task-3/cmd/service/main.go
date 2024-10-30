d046a6522e555da5e91d1deb95ea06ac7c9a3b4b
artem6554 <artem6554@gmail.com>
2024-10-30 23:31:52 +0300

    [TASK-3] added writing to json

--- mesropyan.artyom/task-3/cmd/service/main.go
+++ mesropyan.artyom/task-3/cmd/service/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/artem6554/task-3/config"
 	"github.com/artem6554/task-3/read"
+	"github.com/artem6554/task-3/write"
 )
 
 func main() {
 	config := config.ReadConfig()
 	currencies := read.ParseXML(config.InputFile)
-
 	read.SortValutes(&currencies)
+	write.WriteToJson(currencies, config.OutputFile)
 
 }
