6a1e8e638684fd1c667f5639ed9273997cd57263
sssidkn <sonyadidkina0307@gmail.com>
2024-10-31 12:50:28 +0300

    [TASK-3] added a temporary structure for converting string to float

--- sofia.dudkina/task-3/internal/entities/valute.go
+++ sofia.dudkina/task-3/internal/entities/valute.go
@@ -1,29 +1,35 @@
 package entities
 
+type OutputData struct {
+	Valute []struct {
+		NumCode  int     `json:"num_code,omitempty"`
+		CharCode string  `json:"char_code,omitempty"`
+		Value    float64 `json:"value,omitempty"`
+	}
+}
+
 type CursData struct {
-	ValCurs struct {
-		Date   string `xml:"Date,attr"`
-		Name   string `xml:"name,attr"`
-		Valute []struct {
-			ID        string  `xml:"ID,attr" json:"-"`
-			NumCode   int     `xml:"NumCode" json:"num_code,omitempty"`
-			CharCode  string  `xml:"CharCode" json:"char_code,omitempty"`
-			Nominal   int     `xml:"Nominal" json:"-"`
-			Name      string  `xml:"Name" json:"-"`
-			Value     float64 `xml:"Value" json:"value,omitempty"`
-			VunitRate float64 `xml:"VunitRate" json:"-"`
-		} `xml:"Valute" json:"Valute,omitempty"`
-	} `xml:"ValCurs" json:"ValCurs,omitempty"`
+	Date   string `xml:"Date,attr"`
+	Name   string `xml:"name,attr"`
+	Valute []struct {
+		ID        string `xml:"ID,attr"`
+		NumCode   int    `xml:"NumCode"`
+		CharCode  string `xml:"CharCode"`
+		Nominal   int    `xml:"Nominal"`
+		Name      string `xml:"Name"`
+		Value     string `xml:"Value"`
+		VunitRate string `xml:"VunitRate"`
+	}
 }
 
-func (cursData CursData) Len() int {
-	return len(cursData.ValCurs.Valute)
+func (outData OutputData) Len() int {
+	return len(outData.Valute)
 }
 
-func (cursData CursData) Less(i, j int) bool {
-	return cursData.ValCurs.Valute[i].Value > cursData.ValCurs.Valute[j].Value
+func (outData OutputData) Less(i, j int) bool {
+	return outData.Valute[i].Value > outData.Valute[j].Value
 }
 
-func (cursData CursData) Swap(i, j int) {
-	cursData.ValCurs.Valute[i], cursData.ValCurs.Valute[j] = cursData.ValCurs.Valute[j], cursData.ValCurs.Valute[i]
+func (outData OutputData) Swap(i, j int) {
+	outData.Valute[i], outData.Valute[j] = outData.Valute[j], outData.Valute[i]
 }
--- sofia.dudkina/task-3/internal/parser/parser.go
+++ sofia.dudkina/task-3/internal/parser/parser.go
@@ -6,35 +6,61 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/sssidkn/task-3/internal/entities"
 	"golang.org/x/net/html/charset"
 )
 
-func ParseFile(path string) (*entities.CursData, error) {
+func ParseFile(path string) (*entities.OutputData, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	data = bytes.ReplaceAll(data, []byte(","), []byte("."))
 	extension := filepath.Ext(path)
-	cursData := &entities.CursData{}
+	outData := &entities.OutputData{}
 	switch extension {
 	case ".xml":
-		cursData, err = ParseXML(data)
+		cursData := &entities.CursData{}
+		cursData, err = parseXML(data)
+		if err != nil {
+			return nil, err
+		}
+		outData, err = dataTransfer(cursData)
+		if err != nil {
+			return nil, err
+		}
 	default:
 		return nil, fmt.Errorf("unsupported file extension: %s", extension)
 	}
-	return cursData, err
+	return outData, err
 }
 
-func ParseXML(data []byte) (*entities.CursData, error) {
+func parseXML(data []byte) (*entities.CursData, error) {
 	cursData := new(entities.CursData)
 	decoder := xml.NewDecoder(bytes.NewReader(data))
 	decoder.CharsetReader = charset.NewReaderLabel
-	err := decoder.Decode(&cursData.ValCurs)
+	err := decoder.Decode(&cursData)
 	if err != nil {
 		return nil, err
 	}
 	return cursData, nil
+}
+
+func dataTransfer(cursData *entities.CursData) (*entities.OutputData, error) {
+	outData := &entities.OutputData{}
+	for i := range cursData.Valute {
+		replacedValue := strings.ReplaceAll(cursData.Valute[i].Value, ",", ".")
+		value, err := strconv.ParseFloat(replacedValue, 64)
+		if err != nil {
+			return nil, err
+		}
+		outData.Valute = append(outData.Valute, struct {
+			NumCode  int     `json:"num_code,omitempty"`
+			CharCode string  `json:"char_code,omitempty"`
+			Value    float64 `json:"value,omitempty"`
+		}{NumCode: cursData.Valute[i].NumCode, CharCode: cursData.Valute[i].CharCode, Value: value})
+	}
+	return outData, nil
 }
