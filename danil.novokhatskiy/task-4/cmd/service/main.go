f317ebe2b25f9ff58365ab8b8bffa9db22dac060
katagiri <karuk.vova2005@mail.ru>
2024-11-02 21:05:59 +0300

    [TASK-4] Moved SaveCounter to separated file

--- danil.novokhatskiy/task-4/cmd/service/main.go
+++ danil.novokhatskiy/task-4/cmd/service/main.go
@@ -3,33 +3,12 @@ package main
 import (
 	"fmt"
 	"sync"
+
+	"github.com/katagiriwhy/task-4/internal"
 )
 
-type SaveCounter struct {
-	Mutex   sync.Mutex
-	Counter int
-}
-
-func (c *SaveCounter) Increment() {
-	c.Mutex.Lock()
-	defer c.Mutex.Unlock()
-	c.Counter++
-}
-
-func (c *SaveCounter) Decrement() {
-	c.Mutex.Lock()
-	defer c.Mutex.Unlock()
-	c.Counter--
-}
-
-func (c *SaveCounter) Value() int {
-	c.Mutex.Lock()
-	defer c.Mutex.Unlock()
-	return c.Counter
-}
-
 func main() {
-	counter := SaveCounter{}
+	counter := internal.SaveCounter{}
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
