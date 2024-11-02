08ee6dcd2b7ec6f93c618e92f14af2bb4920c615
artem6554 <artem6554@gmail.com>
2024-11-02 18:49:56 +0300

    [TASK-3] removed unnecessary comments from structs

--- mesropyan.artyom/task-3/structs/valCurs.go
+++ mesropyan.artyom/task-3/structs/valCurs.go
@@ -1,8 +1,5 @@
 package structs
 
 type ValCurs struct {
-	// XMLName xml.Name `xml:"ValCurs"`
-	// Date    string   `xml:"Date",attr`
-	// Name    string   `xml:"name",attr`
 	Valutes []Valute `xml:"Valute"`
 }
--- mesropyan.artyom/task-3/structs/valute.go
+++ mesropyan.artyom/task-3/structs/valute.go
@@ -4,8 +4,4 @@ type Valute struct {
 	NumCode  int     `xml:"NumCode" json:"num_code"`
 	CharCode string  `xml:"CharCode" json:"char_code"`
 	Value    float32 `xml:"Value" json:"value"`
-	//	ID        int    `xml:"ID,attr"`
-	//	VunitRate int    `xml:"VunitRate"`
-	// Nominal   int    `xml:"Nominal"`
-	// Name      string `xml:"Name"`
 }
