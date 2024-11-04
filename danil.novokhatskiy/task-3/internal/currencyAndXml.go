1b2cc7d0db9dee2f8a3992f118af88dab1620834
katagiri <karuk.vova2005@mail.ru>
2024-11-04 22:06:59 +0300

    [TASK-3] Add implementation UnmarshalXML to my own type

--- danil.novokhatskiy/task-3/internal/currencyAndXml.go
+++ danil.novokhatskiy/task-3/internal/currencyAndXml.go
@@ -10,24 +10,29 @@ import (
 	"golang.org/x/net/html/charset"
 )
 
-type CurrencyRaw struct {
-	NumCode  string `xml:"NumCode" json:"num_code"`
-	CharCode string `xml:"CharCode" json:"char_code"`
-	Value    string `xml:"Value" json:"value"`
+type MyFloat float64
+
+func (f *MyFloat) UnmarshalXML(d *xml.Decoder, s xml.StartElement) error {
+	var data string
+	if err := d.DecodeElement(&data, &s); err != nil {
+		return err
+	}
+	value, err := strconv.ParseFloat(strings.ReplaceAll(string(data), ",", "."), 64)
+	if err != nil {
+		return err
+	}
+	*f = MyFloat(value)
+	return nil
 }
 
 type Currency struct {
 	NumCode  string  `xml:"NumCode" json:"num_code"`
 	CharCode string  `xml:"CharCode" json:"char_code"`
-	Value    float64 `xml:"Value" json:"value"`
+	Value    MyFloat `xml:"Value" json:"value"`
 }
 
 type Currencies struct {
 	Currencies []Currency `xml:"Valute"`
-}
-
-type CurrenciesRaw struct {
-	Currencies []CurrencyRaw `xml:"Valute"`
 }
 
 func ParseXml(cur *Currencies, file string) error {
@@ -35,22 +40,9 @@ func ParseXml(cur *Currencies, file string) error {
 	if err != nil {
 		return err
 	}
-	var tmp CurrenciesRaw
 	decoder := xml.NewDecoder(bytes.NewReader(data))
 	decoder.CharsetReader = charset.NewReaderLabel
-	err = decoder.Decode(&tmp)
-	cur.Currencies = make([]Currency, len(tmp.Currencies))
-	for i := 0; i < len(tmp.Currencies); i++ {
-		value, err := strconv.ParseFloat(strings.ReplaceAll(tmp.Currencies[i].Value, ",", "."), 64)
-		if err != nil {
-			return err
-		}
-		cur.Currencies[i] = Currency{
-			NumCode:  tmp.Currencies[i].NumCode,
-			CharCode: tmp.Currencies[i].CharCode,
-			Value:    value,
-		}
-	}
+	err = decoder.Decode(&cur)
 	if err != nil {
 		return err
 	}
