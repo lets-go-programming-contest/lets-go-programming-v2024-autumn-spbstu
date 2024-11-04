1be9948745f0f91043c4028d3f5c3b39177ad674
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-11-04 18:39:21 +0300

    [TASK-3] Add Default Config Path

--- andrey.kostin/task-3/internal/config/flag.go
+++ andrey.kostin/task-3/internal/config/flag.go
@@ -6,7 +6,7 @@ import (
 
 func GetConfigPath() string {
 	var configPath string
-	flag.StringVar(&configPath, "config", "", "path to the configuration file")
+	flag.StringVar(&configPath, "config", "config.yaml", "path to the configuration file")
 	flag.Parse()
 	return configPath
 }
