ba6206fc0954c46883892d50eaaaceabfa8073a5
sssidkn <sonyadidkina0307@gmail.com>
2024-11-04 20:34:58 +0300

    [TASK-4] add testing functions

--- sofia.dudkina/task-4/cmd/service/main.go
+++ sofia.dudkina/task-4/cmd/service/main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-
-	"github.com/sssidkn/task-4/internal/entity"
-)
+import "github.com/sssidkn/task-4/internal/test"
 
 func main() {
-	store := &entity.Store{}
-	wg := &sync.WaitGroup{}
-	store.Products = map[string]int{"Milk": 10, "Apple": 2, "Orange": 3}
-	for i := 1; i < 5; i++ {
-		wg.Add(1)
-		go func() {
-			err := store.SellProduct("Orange", i, wg)
-			if err != nil {
-				fmt.Println(err)
-			}
-		}()
-	}
-	for i := 1; i < 5; i++ {
-		wg.Add(1)
-		go store.AddProduct("Orange", i, wg)
-	}
-	wg.Wait()
+	test.SafeTest()
 }
--- sofia.dudkina/task-4/internal/entity/unsafeStore.go
+++ sofia.dudkina/task-4/internal/entity/unsafeStore.go
@@ -6,14 +6,11 @@ import (
 	"sync"
 )
 
-type Store struct {
+type UnsafeStore struct {
 	Products map[string]int
-	mutex    sync.Mutex
 }
 
-func (s *Store) AddProduct(name string, count int, wg *sync.WaitGroup) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+func (s *UnsafeStore) Add(name string, count int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	if _, ok := s.Products[name]; ok {
 		s.Products[name] += count
@@ -23,9 +20,7 @@ func (s *Store) AddProduct(name string, count int, wg *sync.WaitGroup) {
 	fmt.Printf("Product %s added to store. Count = %d\n", name, s.Products[name])
 }
 
-func (s *Store) SellProduct(name string, sellCount int, wg *sync.WaitGroup) error {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+func (s *UnsafeStore) Sell(name string, sellCount int, wg *sync.WaitGroup) error {
 	defer wg.Done()
 	productCount, ok := s.Products[name]
 	if !ok {
@@ -35,6 +30,7 @@ func (s *Store) SellProduct(name string, sellCount int, wg *sync.WaitGroup) erro
 	if productCount-sellCount < 0 {
 		return fmt.Errorf("product %s not enough to sell %d. Count = %d", name, sellCount, productCount)
 	}
+
 	s.Products[name] -= sellCount
 	fmt.Printf("Product %s sold from store. Count = %d\n", name, s.Products[name])
 	return nil
