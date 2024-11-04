fb6b4f190edb10897d94a7dcab7155944c8df8ef
Mark Zheleznyakov <mrqiz@internet.ru>
2024-11-04 13:57:50 +0300

    [TASK-4] Added Push and Pop methods for the Parking cars

--- mark.zheleznyakov/task-4/internal/parking/parking.go
+++ mark.zheleznyakov/task-4/internal/parking/parking.go
@@ -1,20 +1,42 @@
 package parking
 
 import (
-    "sync"
+  "fmt"
+	"sync"
 )
 
 type Parking struct {
-  mu sync.Mutex
-  reserved int
-  capacity int
+	mu       sync.Mutex
+	reserved int
+	capacity int
 }
 
 func NewParking(capacity int) *Parking {
-  return &Parking{capacity: capacity}
+	return &Parking{capacity: capacity}
 }
 
 func (p *Parking) Capacity() int {
-  return p.capacity
+  p.mu.Lock()
+  defer p.mu.Unlock()
+	return p.capacity
 }
 
+func (p *Parking) PushCar() error {
+  p.mu.Lock()
+  defer p.mu.Unlock()
+  if p.reserved >= p.capacity {
+    return fmt.Errorf("cannot push car: parking is full")
+  }
+  p.reserved++
+	return nil
+}
+
+func (p *Parking) PopCar() error {
+  p.mu.Lock()
+  defer p.mu.Unlock()
+  if p.reserved <= 0 {
+    return fmt.Errorf("cannot pop car: parking is empty")
+  }
+  p.reserved--
+	return nil
+}
