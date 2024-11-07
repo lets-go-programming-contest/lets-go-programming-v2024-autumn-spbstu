e029ec4a802a3601a2b76872197631ba8b8a974f
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-11-07 23:04:00 +0300

    [TASK-3] add ExportToJSON method for CurrencyRates

--- kirill.romanchuk/task-3/cmd/service/main.go
+++ kirill.romanchuk/task-3/cmd/service/main.go
@@ -13,22 +13,9 @@ func main() {
 
 	if err != nil {
 		fmt.Print(err)
-	} else {
-		fmt.Println(config.InputFile)
-		fmt.Println(config.OutputFile)
 	}
-
 	currencyRates := strtructs.CurrencyRates{}
 	currencyRates.ParseXML(config.InputFile)
-	fmt.Println(currencyRates.Date)
-	for _, currency := range currencyRates.Currencies {
-		fmt.Printf("NumCode: %d, CharCode: %s, Nominal: %d, Name: %s, Value: %.4f\n",
-			currency.NumCode, currency.CharCode, currency.Nominal, currency.Name, currency.Value)
-	}
-	fmt.Println("////////////////////////////////////////////////////////////////////")
 	currencyRates.SortByValue(true)
-	for _, currency := range currencyRates.Currencies {
-		fmt.Printf("NumCode: %d, CharCode: %s, Nominal: %d, Name: %s, Value: %.4f\n",
-			currency.NumCode, currency.CharCode, currency.Nominal, currency.Name, currency.Value)
-	}
+	currencyRates.ExportSelectedCurrencyRatesToJSON(config.OutputFile)
 }
--- kirill.romanchuk/task-3/strtructs/сurrencyRates.go
+++ kirill.romanchuk/task-3/strtructs/сurrencyRates.go
@@ -1,6 +1,7 @@
 package strtructs
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"os"
 	"sort"
@@ -40,4 +41,52 @@ func (c *CurrencyRates) SortByValue(reverse bool) {
 			return c.Currencies[i].Value < c.Currencies[j].Value
 		})
 	}
+}
+
+func (c *CurrencyRates) ExportSelectedCurrencyRatesToJSON(filename string, fields ...string) error {
+	var output []map[string]interface{}
+
+	for _, currency := range c.Currencies {
+		data := make(map[string]interface{})
+
+		if len(fields) == 0 {
+			data["NumCode"] = currency.NumCode
+			data["CharCode"] = currency.CharCode
+			data["Nominal"] = currency.Nominal
+			data["Name"] = currency.Name
+			data["Value"] = currency.Value
+			data["VunitRate"] = currency.VunitRate
+		} else {
+			for _, field := range fields {
+				switch field {
+				case "NumCode":
+					data["NumCode"] = currency.NumCode
+				case "CharCode":
+					data["CharCode"] = currency.CharCode
+				case "Nominal":
+					data["Nominal"] = currency.Nominal
+				case "Name":
+					data["Name"] = currency.Name
+				case "Value":
+					data["Value"] = currency.Value
+				case "VunitRate":
+					data["VunitRate"] = currency.VunitRate
+				}
+			}
+		}
+
+		output = append(output, data)
+	}
+
+	jsonData, err := json.MarshalIndent(output, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filename, jsonData, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
