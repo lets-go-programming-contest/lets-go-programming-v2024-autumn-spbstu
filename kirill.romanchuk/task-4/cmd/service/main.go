d2df228ac65b1a3ba1410e4bd62b08d3770cbf46
Kirill <kirillromancuk72@gmail.com>
2024-11-11 02:03:48 +0300

    [TASK-4] Update main

--- kirill.romanchuk/task-4/cmd/service/main.go
+++ kirill.romanchuk/task-4/cmd/service/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"sync"
-	"time"
 
+	"github.com/kirill.romanchuk/task-4/structures/client"
 	"github.com/kirill.romanchuk/task-4/structures/performance"
+	"github.com/kirill.romanchuk/task-4/utils/readers"
 )
 
 func main() {
+	var wGroup sync.WaitGroup
 	var mutex sync.Mutex
-	perm := performance.CreatePerformance(3, 4)
+	rows, cols := readers.ReadIntNum("Введите количисво рядов (1-3) ", 1, 3), readers.ReadIntNum("Введите количисво мест в ряду (1-3)", 1, 3)
+	perm := performance.CreatePerformance(rows, cols)
 	perm.DisplaySeats()
-	go perm.ReserveSeat(1, 1, &mutex)
-	go perm.ReserveSeat(1, 1, &mutex)
-	time.Sleep(5 * time.Second)
+
+	numClients := readers.ReadIntNum("Введите количисво людей желающих забронировать место (1-16) ", 1, 16)
+	wGroup.Add(numClients)
+	for i := 1; i <= numClients; i++ {
+		client := client.Client{ID: i}
+		go client.ReserveSeat(&perm, &mutex, &wGroup)
+	}
+
+	wGroup.Wait()
 	perm.DisplaySeats()
-	time.Sleep(5 * time.Second)
 }
