9c79fb1b596af6bdb200824a552e6f1c1d12bb76
Kirill <kirillromancuk72@gmail.com>
2024-11-11 00:31:21 +0300

    [TASK-4] add ReserveSeat func

--- kirill.romanchuk/task-4/cmd/service/main.go
+++ kirill.romanchuk/task-4/cmd/service/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"sync"
+	"time"
+
 	"github.com/kirill.romanchuk/task-4/structures/performance"
 )
 
 func main() {
+	var mutex sync.Mutex
 	perm := performance.CreatePerformance(3, 4)
 	perm.DisplaySeats()
+	go perm.ReserveSeat(1, 1, &mutex)
+	go perm.ReserveSeat(1, 1, &mutex)
+	time.Sleep(5 * time.Second)
+	perm.DisplaySeats()
+	time.Sleep(5 * time.Second)
 }
--- kirill.romanchuk/task-4/structures/performance/performance.go
+++ kirill.romanchuk/task-4/structures/performance/performance.go
@@ -2,11 +2,12 @@ package performance
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 type Performance struct {
-	Seats          [][]int
-	availableSeats int
+	Seats [][]int
 }
 
 func CreatePerformance(rows, cols int) Performance {
@@ -17,11 +18,20 @@ func CreatePerformance(rows, cols int) Performance {
 			seats[i][j] = 0
 		}
 	}
-	return Performance{Seats: seats, availableSeats: rows * cols}
+	return Performance{Seats: seats}
 }
 
 func (p Performance) DisplaySeats() {
 	for _, row := range p.Seats {
 		fmt.Println(row)
 	}
+}
+
+func (p *Performance) ReserveSeat(row, col int, mutex *sync.Mutex) {
+	mutex.Lock()
+	if p.Seats[row][col] == 0 {
+		time.Sleep(2 * time.Second) // Simulated payment
+		p.Seats[row][col] += 1
+	}
+	mutex.Unlock()
 }
