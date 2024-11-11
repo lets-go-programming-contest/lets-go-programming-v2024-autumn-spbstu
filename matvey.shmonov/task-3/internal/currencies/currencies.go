030c103f173a0d8d471ddaea230017c37871ce90
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-11-11 21:27:25 +0300

    [TASK-3] Implemented TextUnmarshaler interface for Float64 to read comma delimited numbers

--- matvey.shmonov/task-3/internal/currencies/currencies.go
+++ matvey.shmonov/task-3/internal/currencies/currencies.go
@@ -5,7 +5,7 @@ import "sort"
 type Currency struct {
 	NumCode  string  `xml:"NumCode" json:"num_code"`
 	CharCode string  `xml:"CharCode" json:"char_code"`
-	Value    float64 `xml:"Value" json:"value"`
+	Value    Float64 `xml:"Value" json:"value"`
 }
 
 type Currencies struct {
--- matvey.shmonov/task-3/internal/currencies/xml.go
+++ matvey.shmonov/task-3/internal/currencies/xml.go
@@ -6,9 +6,23 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 
 	"golang.org/x/text/encoding/charmap"
 )
+
+type Float64 float64
+func (f *Float64) UnmarshalText(text []byte) error {
+	strValue := string(text)
+	strValue = strings.ReplaceAll(strValue, ",", ".")
+	value, err := strconv.ParseFloat(strValue, 64)
+	if err != nil {
+		return err
+	}
+	*f = Float64(value)
+	return nil
+}
 
 func UnmarshalXML(c *Currencies, f string) error {
 	text, err := os.ReadFile(f)
@@ -16,7 +30,6 @@ func UnmarshalXML(c *Currencies, f string) error {
 		return fmt.Errorf("your input file is cooked: %w", err)
 	}
 
-	text = bytes.ReplaceAll(text, []byte(","), []byte("."))
 	decoder := xml.NewDecoder(bytes.NewReader(text))
 	decoder.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
 		if charset == "windows-1251" {
