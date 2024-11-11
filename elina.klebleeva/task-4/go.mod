3c1c9acfb07790b4573c4adf331fad8c646e9172
EmptyInsid <elinaklebleeva001@gmail.com>
2024-11-11 17:31:18 +0300

    [TASK-5] init commit

--- elina.klebleeva/task-4/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/EmptyInsid/task-4
-
-go 1.23.2
--- elina.klebleeva/task-4/internal/interfaces/postSocialNet/postSocialNet.go
+++
@@ -1,7 +0,0 @@
-package postSocialNet
-
-type PostSocialNet interface {
-	AddTextContent(newText string)
-	RemoveTextContent(oldText string)
-	GetTextContent() string
-}
