b0d6237604d4a1d35c4620647ad922f39c756ae6
Gleb Zhkov <ququshka 008 dog gmail dot com>
2024-11-16 22:51:11 +0300

    [TASK-7] squash

---
+++ zhukov.gleb/task-7/cmd/db/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+
+	dbPack "task-7/internal/db"
+)
+
+func main() {
+	connStr := "user=username dbname=mydb sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dbService := dbPack.New(db)
+
+	names, err := dbService.GetNames()
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
