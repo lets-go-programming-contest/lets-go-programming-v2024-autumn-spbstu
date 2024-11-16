7fd1dcdc56247f3942120aaf67c9f49db4e50196
Gleb Zhkov <ququshka 008 dog gmail dot com>
2024-11-16 22:51:11 +0300

    [TASK-7] first commit

---
+++ zhukov.gleb/task-7/cmd/db/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	dbPack "task-5/internal/db"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	connStr := "user=username dbname=mydb sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dbService := dbPack.New(db)
+
+	names, err := dbService.GetNames()
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
---
+++ zhukov.gleb/task-7/internal/db/db_functions_test.go
@@ -0,0 +1,168 @@
+package db_test
+
+import (
+	"errors"
+	"testing"
+
+	"task-5/internal/db"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/require"
+)
+
+// go test -coverpkg=./... -coverprofile cover.out ./...
+// go tool cover -html cover.out -o cover.html
+
+type rowTestDb struct {
+	names       []string
+	errExpected error
+}
+
+var testTable = []rowTestDb{
+	{
+		names: []string{"Ivan, Gena228"},
+	},
+	{
+		names:       nil,
+		errExpected: errors.New("ExpectedError"),
+	},
+}
+
+func TestGetName(t *testing.T) {
+	t.Parallel()
+
+	t.Run("success GetNames()", func(t *testing.T) {
+		mockDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when marshaling expected json data", err)
+		}
+		dbService := db.DBService{DB: mockDB}
+
+		for i, row := range testTable {
+			mock.ExpectQuery("SELECT name FROM users").
+				WillReturnRows(mockDbRows(row.names)).
+				WillReturnError(row.errExpected)
+			names, err := dbService.GetNames()
+			if row.errExpected != nil {
+				require.ErrorIs(t, err, row.errExpected, "row: %d, expected error: %w, actual error: %w ",
+					i,
+					row.errExpected,
+					err,
+				)
+				require.Nil(t, names, "row: %d, names must be nil", i)
+				continue
+			}
+			require.NoError(t, err, "row: %d, error must be nil", i)
+			require.Equal(t, row.names, names, "row: %d, expected names: %s, actual names: %s ", i,
+				row.names, names)
+		}
+	})
+
+	t.Run("rows.Scan(&name) err", func(t *testing.T) {
+		mockDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when marshaling expected json data", err)
+		}
+		dbService := db.New(mockDB)
+
+		mock.ExpectQuery("SELECT name FROM users").
+			WillReturnRows(sqlmock.NewRows([]string{"name"}).
+				AddRow(nil))
+		_, err = dbService.GetNames()
+
+		require.Error(t, err, "error must be not nil, got %v", err)
+	})
+
+	t.Run("rows.Err()", func(t *testing.T) {
+		mockDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when marshaling expected json data", err)
+		}
+		dbService := db.New(mockDB)
+
+		mock.ExpectQuery("SELECT name FROM users").
+			WillReturnRows(sqlmock.NewRows([]string{"name"}).
+				AddRow("one").
+				AddRow("two").
+				RowError(1, sqlmock.ErrCancelled),
+			)
+
+		_, err = dbService.GetNames()
+
+		require.Error(t, err, "error must be not nil, got %v", err)
+	})
+}
+
+func TestSelectUniqueValues(t *testing.T) {
+	t.Parallel()
+
+	t.Run("success SelectUniqueValues()", func(t *testing.T) {
+		mockDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when marshaling expected json data", err)
+		}
+		dbService := db.DBService{DB: mockDB}
+
+		for i, row := range testTable {
+			mock.ExpectQuery("SELECT DISTINCT name FROM users").
+				WillReturnRows(mockDbRows(row.names)).
+				WillReturnError(row.errExpected)
+			names, err := dbService.SelectUniqueValues("name", "users")
+			if row.errExpected != nil {
+				require.ErrorIs(t, err, row.errExpected, "row: %d, expected error: %w, actual error: %w ",
+					i,
+					row.errExpected,
+					err,
+				)
+				require.Nil(t, names, "row: %d, names must be nil", i)
+				continue
+			}
+			require.NoError(t, err, "row: %d, error must be nil", i)
+			require.Equal(t, row.names, names, "row: %d, expected names: %s, actual names: %s ", i,
+				row.names, names)
+		}
+	})
+
+	t.Run("rows.Scan(&value) err", func(t *testing.T) {
+		mockDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when marshaling expected json data", err)
+		}
+		dbService := db.New(mockDB)
+
+		mock.ExpectQuery("SELECT DISTINCT name FROM users").
+			WillReturnRows(sqlmock.NewRows([]string{"name"}).
+				AddRow(nil))
+
+		_, err = dbService.SelectUniqueValues("name", "users")
+
+		require.Error(t, err, "error must be not nil, got %v", err)
+	})
+
+	t.Run("rows.Err()", func(t *testing.T) {
+		mockDB, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when marshaling expected json data", err)
+		}
+		dbService := db.New(mockDB)
+
+		mock.ExpectQuery("SELECT DISTINCT name FROM users").
+			WillReturnRows(sqlmock.NewRows([]string{"name"}).
+				AddRow("one").
+				AddRow("two").
+				RowError(1, sqlmock.ErrCancelled),
+			)
+
+		_, err = dbService.SelectUniqueValues("name", "users")
+
+		require.Error(t, err, "error must be not nil, got %v", err)
+	})
+}
+
+func mockDbRows(names []string) *sqlmock.Rows {
+	rows := sqlmock.NewRows([]string{"name"})
+	for _, name := range names {
+		rows = rows.AddRow(name)
+	}
+	return rows
+}
