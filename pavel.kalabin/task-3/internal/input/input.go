56ce2eb99e33d55b281719d3c97fc7e7ada6cbcc
Pavel Kalabin <kalabin.pp@edu.spbstu.ru>
2024-11-17 21:47:10 +0300

    [TASK-3] Fix file permissions

--- pavel.kalabin/task-3/internal/input/input.go
+++ pavel.kalabin/task-3/internal/input/input.go
@@ -12,17 +12,21 @@ func ReadXML(FilePath string) (contents []byte, err error) {
 
 func WriteResult(FilePath string, Result []byte) (err error) {
 
+    const (
+        DirPerm = 0755
+        FilePerm = 0644
+    )
     directory := filepath.Dir(FilePath)
-    err = os.MkdirAll(directory, os.ModePerm)
+    err = os.MkdirAll(directory, DirPerm)
     if err != nil {
         return 
     }
-    file, err := os.OpenFile(FilePath, os.O_CREATE|os.O_RDWR, 0644)
+    file, err := os.OpenFile(FilePath, os.O_CREATE|os.O_RDWR, FilePerm)
     if err != nil {
         return
     }
     defer file.Close()
-    os.WriteFile(file.Name(), Result, os.ModePerm)
+    os.WriteFile(file.Name(), Result, FilePerm)
     if err != nil {
         return
     }
