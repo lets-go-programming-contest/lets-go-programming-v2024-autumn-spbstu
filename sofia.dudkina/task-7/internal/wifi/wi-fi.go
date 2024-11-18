09857e37b9c3aeb3f712ae46b0915fd59070df40
sssidkn <sonyadidkina0307@gmail.com>
2024-11-18 20:28:52 +0300

    [TASK-7] CI attempt-3

--- sofia.dudkina/task-7/internal/wifi/wi-fi.go
+++ sofia.dudkina/task-7/internal/wifi/wi-fi.go
@@ -17,32 +17,51 @@ type Service struct {
 }
 
 func New(wifi WiFi) Service {
+
 	return Service{WiFi: wifi}
+
 }
 
 func (service Service) GetAddresses() ([]net.HardwareAddr, error) {
+
 	interfaces, err := service.WiFi.Interfaces()
+
 	if err != nil {
+
 		return nil, err
+
 	}
+
 	var addrs []net.HardwareAddr
 
 	for _, iface := range interfaces {
+
 		addrs = append(addrs, iface.HardwareAddr)
+
 	}
+
 	return addrs, nil
+
 }
 
 func (service Service) GetNames() ([]string, error) {
 
 	interfaces, err := service.WiFi.Interfaces()
+
 	if err != nil {
+
 		return nil, err
+
 	}
+
 	var nameList []string
 
 	for _, iface := range interfaces {
+
 		nameList = append(nameList, iface.Name)
+
 	}
+
 	return nameList, nil
+
 }
