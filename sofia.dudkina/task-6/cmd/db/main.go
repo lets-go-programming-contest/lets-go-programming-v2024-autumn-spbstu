774f5d24d63a2098ff801bb0f0760ca16be0548d
sssidkn <sonyadidkina0307@gmail.com>
2024-11-18 20:22:11 +0300

    [TASK-6] fix linting#1

--- sofia.dudkina/task-6/cmd/db/main.go
+++ sofia.dudkina/task-6/cmd/db/main.go
@@ -2,6 +2,40 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+
+	dbPack "example_mock/internal/db"
+)
+
+func main() {
+
+	connStr := "user=username dbname=mydb sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+
+		log.Fatal(err)
+
+	}
+
+	defer db.Close()
+
+	dbService := dbPack.New(db)
+
+	names, err := dbService.GetNames()
+
+	for _, name := range names {
+
+		fmt.Println(name)
+
+	}
+
+}
+
+import (
+	"database/sql"
 	dbPack "example_mock/internal/db"
 	"fmt"
 	"log"
--- sofia.dudkina/task-6/cmd/wifi/main.go
+++ sofia.dudkina/task-6/cmd/wifi/main.go
@@ -1,6 +1,45 @@
 package main
 
 import (
+	"fmt"
+
+	myWifi "example_mock/internal/wifi"
+	"github.com/mdlayher/wifi"
+)
+
+func main() {
+
+	wifiClient, err := wifi.New()
+
+	if err != nil {
+
+		fmt.Printf("Ошибка при создании wifiClient: %s\n", err.Error())
+
+		return
+
+	}
+
+	wifiService := myWifi.New(wifiClient)
+
+	addrs, err := wifiService.GetAddresses()
+
+	if err != nil {
+
+		fmt.Printf("Ошибка при получении адресов: %s\n", err.Error())
+
+		return
+
+	}
+
+	for _, addr := range addrs {
+
+		fmt.Println(addr)
+
+	}
+
+}
+
+import (
 	myWifi "example_mock/internal/wifi"
 	"fmt"
 
