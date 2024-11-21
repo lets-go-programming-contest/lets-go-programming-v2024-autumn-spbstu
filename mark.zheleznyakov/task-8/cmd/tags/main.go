78a80b80c8b9b52ff5f9a8421d56e799b979d733
Mark Zheleznyakov <mrqiz@internet.ru>
2024-11-21 15:47:47 +0300

    [TASK-8] [tags] Added calculations exec

--- mark.zheleznyakov/task-8/cmd/tags/main.go
+++ mark.zheleznyakov/task-8/cmd/tags/main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "os"
+  "strconv"
 
 	"github.com/mrqiz/task-8/internal/feats"
 	"github.com/mrqiz/task-8/internal/output"
@@ -16,11 +17,21 @@ func main() {
     os.Exit(1)
   }
 
-  featName := os.Args[1]
-  if feats.FindByName(featName) == nil {
+  feat := feats.FindByName(os.Args[1])
+  if feat == nil {
     output.CliOpNotFound()
     os.Exit(1)
   }
 
-  fmt.Println(os.Args[2:])
+  args := make([]float64, len(os.Args[2:]))
+
+  for i, str := range os.Args[2:] {
+		if value, err := strconv.ParseFloat(str, 64); err == nil {
+			args[i] = value
+		} else {
+			fmt.Printf("the '%s' arg is not... you know... convertible 🚗\n", str)
+		}
+	}
+
+  fmt.Println(feat.Exec(args...))
 }
