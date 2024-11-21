89c318b3fe664f53f16b8180316f8d26b4ca5be9
Aleksandr Udalov <udalov.aa@edu.spbstu.ru>
2024-11-21 12:09:57 +0300

    Fix problems with mockery

--- alex.udalov/task-6/go.mod
+++ alex.udalov/task-6/go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/mdlayher/netlink v1.7.2 // indirect
 	github.com/mdlayher/socket v0.4.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/crypto v0.10.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
---
+++ alex.udalov/task-6/internal/wifi/wi-fi_test.go
@@ -0,0 +1,83 @@
+package wifi_test
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	myWifi "task-6/internal/wifi"
+	"testing"
+
+	"github.com/mdlayher/wifi"
+	"github.com/stretchr/testify/require"
+)
+
+type rowTestSysInfo struct {
+	addrs       []string
+	errExpected error
+}
+
+var testTable = []rowTestSysInfo{
+	{
+		addrs: []string{"00:11:22:33:44:55", "aa:bb:cc:dd:ee:ff"},
+	},
+	{
+		errExpected: errors.New("ExpectedError"),
+	},
+}
+
+func TestGetNames(t *testing.T) {
+	t.Parallel()
+
+	mockWifi := NewWiFi(t)
+	wifiService := myWifi.Service{WiFi: mockWifi}
+
+	for i, row := range testTable {
+		mockWifi.On("Interfaces").Unset()
+		mockWifi.On("Interfaces").Return(mockIfaces(row.addrs), row.errExpected)
+		actualNames, err := wifiService.GetNames()
+
+		if row.errExpected != nil {
+			require.ErrorIs(t, err, row.errExpected, "row: %d, expected error: %w, actual error: %w", i, row.errExpected, err)
+			continue
+		}
+
+		require.NoError(t, err, "row: %d, error must be nil", i)
+		require.Equal(t, row.addrs, actualNames, "row: %d, expected names: %s, actual names: %s", i, row.addrs, actualNames)
+	}
+}
+
+func mockIfaces(addrs []string) []*wifi.Interface {
+	var interfaces []*wifi.Interface
+	for i, addrStr := range addrs {
+		hwAddr := parseMAC(addrStr)
+		if hwAddr == nil {
+			continue
+		}
+		iface := &wifi.Interface{
+			Index:        i + 1,
+			Name:         fmt.Sprintf("eth%d", i+1),
+			HardwareAddr: hwAddr,
+			PHY:          1,
+			Device:       1,
+			Type:         wifi.InterfaceTypeAPVLAN,
+			Frequency:    0,
+		}
+		interfaces = append(interfaces, iface)
+	}
+	return interfaces
+}
+func parseMACs(macStr []string) []net.HardwareAddr {
+	var addrs []net.HardwareAddr
+	for _, addr := range macStr {
+		addrs = append(addrs, parseMAC(addr))
+	}
+	return addrs
+}
+
+func parseMAC(macStr string) net.HardwareAddr {
+	hwAddr, err := net.ParseMAC(macStr)
+	if err != nil {
+		return nil
+	}
+	return hwAddr
+}
