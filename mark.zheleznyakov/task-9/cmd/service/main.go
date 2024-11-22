94145b3cab70f630b06a80a87eebda019047c288
Mark Zheleznyakov <mrqiz@internet.ru>
2024-11-22 11:02:21 +0300

    [TASK-9] [database] Contact model

--- mark.zheleznyakov/task-9/cmd/service/main.go
+++ mark.zheleznyakov/task-9/cmd/service/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-
   "github.com/mrqiz/task-9/internal/database"
+  "github.com/mrqiz/task-9/internal/models"
 )
 
 func main() {
   database.Connect()
+  database.DB.AutoMigrate(&models.Contact{})
+  database.DB.Create(&models.Contact{ Name: "Octo8", Phone: "8 (888) 888-88-88" })
 }
