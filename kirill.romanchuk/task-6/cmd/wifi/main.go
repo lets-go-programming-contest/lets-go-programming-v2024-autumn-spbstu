52af2d1860f0420c064c3dad520cc749b0c86a66
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-11-23 01:46:25 +0300

    [TASK-6] fix wifi/main.go for golangci-lint

--- kirill.romanchuk/task-6/cmd/wifi/main.go
+++ kirill.romanchuk/task-6/cmd/wifi/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	myWifi "example_mock/internal/wifi"
 	"fmt"
+
 	"github.com/mdlayher/wifi"
+
+	myWifi "example_mock/internal/wifi"
 )
 
 func main() {
 	wifiClient, err := wifi.New()
 	if err != nil {
 		fmt.Printf("Ошибка при создании wifiClient: %s\n", err.Error())
+
 		return
 	}
 
@@ -18,6 +21,7 @@ func main() {
 	addrs, err := wifiService.GetAddresses()
 	if err != nil {
 		fmt.Printf("Ошибка при получении адресов: %s\n", err.Error())
+
 		return
 	}
 
