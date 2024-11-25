dd6cd5b7f5407465ab245decda1c60bb69eb0d2d
sssidkn <sonyadidkina0307@gmail.com>
2024-11-25 18:46:11 +0300

    [TASK-8] added embed and tags

--- sofia.dudkina/task-8/cmd/embed/main.go
+++ sofia.dudkina/task-8/cmd/embed/main.go
@@ -1,1 +1,14 @@
-package embed
+package main
+
+import _ "embed"
+
+//go:embed src/text1.txt
+var text string
+
+//go:embed src/text2.txt
+var byteText []byte
+
+func main() {
+	println(text)
+	println(string(byteText))
+}
