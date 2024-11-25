5382747d21bc9a391fda1409c56ad2344514fc39
hahapathetic <koraomeilo@gmail.com>
2024-11-25 17:09:42 +0300

    [TASK-7] initial commit

---
+++ alina.mamonova/task-7/cmd/wifi/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	myWifi "github.com/hahapathetic/task-7/internal/wifi"
+	"github.com/mdlayher/wifi"
+)
+
+func main() {
+	wifiClient, err := wifi.New()
+	if err != nil {
+		fmt.Printf("Ошибка при создании wifiClient: %s\n", err.Error())
+		return
+	}
+
+	wifiService := myWifi.New(wifiClient)
+
+	addrs, err := wifiService.GetAddresses()
+	if err != nil {
+		fmt.Printf("Ошибка при получении адресов: %s\n", err.Error())
+		return
+	}
+
+	for _, addr := range addrs {
+		fmt.Println(addr)
+	}
+}
