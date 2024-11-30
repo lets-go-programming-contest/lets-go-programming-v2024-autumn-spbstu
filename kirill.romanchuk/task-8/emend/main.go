fe71e28394717f9f10f490da6aa4315abe5a7214
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-11-30 16:34:41 +0300

    [TASK-8] add task

---
+++ kirill.romanchuk/task-8/emend/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"embed"
+)
+
+//go:embed folder/single_file.txt
+var fileString string
+
+//go:embed folder/single_file.txt
+var fileByte []byte
+
+//go:embed folder/*.hash
+var folder embed.FS
+
+func main() {
+	println(fileString)
+	println(string(fileByte))
+
+	content1, _ := folder.ReadFile("folder/file1.hash")
+	println(string(content1))
+
+	content2, _ := folder.ReadFile("folder/file2.hash")
+	println(string(content2))
+}
