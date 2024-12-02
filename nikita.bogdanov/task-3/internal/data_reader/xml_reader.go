9b5cf542abb08c4df6790f016110a16949cfe667
solomonalfred <nik_bogdanov2002@mail.ru>
2024-12-02 17:08:50 +0300

    [TASK-3] Add UnmarshalXML

--- nikita.bogdanov/task-3/internal/data_reader/xml_reader.go
+++ nikita.bogdanov/task-3/internal/data_reader/xml_reader.go
@@ -1,12 +1,9 @@
 package data_reader
 
 import (
-	"bytes"
 	"encoding/xml"
-	"io"
 	"os"
 	"sort"
-	"strings"
 
 	"github.com/solomonalfred/task-3/internal/schemas"
 	"golang.org/x/text/encoding/charmap"
@@ -19,17 +16,9 @@ func GetValuteCurse(config schemas.ConfigStruct) (*schemas.ValuteCurseStructure,
 		return nil, err
 	}
 	defer file.Close()
-	decoder := charmap.Windows1251.NewDecoder()
-	reader := transform.NewReader(file, decoder)
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-	// data = []byte(data)
-	data = []byte(strings.ReplaceAll(string(data), ",", "."))
-	data = fixXMLPrologEncoding(data)
+	decoder := xml.NewDecoder(transform.NewReader(file, charmap.Windows1251.NewDecoder()))
 	valCurs := new(schemas.ValuteCurseStructure)
-	err = xml.Unmarshal(data, valCurs)
+	err = decoder.Decode(valCurs)
 	if err != nil {
 		return nil, err
 	}
@@ -37,11 +26,4 @@ func GetValuteCurse(config schemas.ConfigStruct) (*schemas.ValuteCurseStructure,
 		return valCurs.Valute[i].Value < valCurs.Valute[j].Value
 	})
 	return valCurs, nil
-}
-
-func fixXMLPrologEncoding(data []byte) []byte {
-	oldEncoding := []byte(`encoding="windows-1251"`)
-	newEncoding := []byte(`encoding="UTF-8"`)
-	data = bytes.Replace(data, oldEncoding, newEncoding, 1)
-	return data
 }
--- nikita.bogdanov/task-3/internal/schemas/valute_structure.go
+++ nikita.bogdanov/task-3/internal/schemas/valute_structure.go
@@ -1,5 +1,12 @@
 package schemas
 
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type ValuteStructure struct {
 	ID        string  `xml:"ID,attr"`
 	NumCode   int     `xml:"NumCode"`
@@ -8,4 +15,40 @@ type ValuteStructure struct {
 	Name      string  `xml:"Name"`
 	Value     float64 `xml:"Value"`
 	VunitRate float64 `xml:"VunitRate"`
+}
+
+func (v *ValuteStructure) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	type Alias ValuteStructure
+	aux := &struct {
+		Value     string `xml:"Value"`
+		VunitRate string `xml:"VunitRate"`
+		*Alias
+	}{
+		Alias: (*Alias)(v),
+	}
+	if err := d.DecodeElement(aux, &start); err != nil {
+		return err
+	}
+	valueStr := strings.ReplaceAll(aux.Value, ",", ".")
+	value, err := strconv.ParseFloat(strings.TrimSpace(valueStr), 64)
+	if err != nil {
+		return fmt.Errorf("error parsing Value: %v", err)
+	}
+	v.Value = value
+	if aux.VunitRate != "" {
+		vunitRateStr := strings.ReplaceAll(aux.VunitRate, ",", ".")
+		vunitRate, err := strconv.ParseFloat(strings.TrimSpace(vunitRateStr), 64)
+		if err != nil {
+			return fmt.Errorf("error parsing VunitRate: %v", err)
+		}
+		v.VunitRate = vunitRate
+	} else {
+		v.VunitRate = 0.0
+	}
+	v.ID = aux.ID
+	v.NumCode = aux.NumCode
+	v.CharCode = aux.CharCode
+	v.Nominal = aux.Nominal
+	v.Name = aux.Name
+	return nil
 }
