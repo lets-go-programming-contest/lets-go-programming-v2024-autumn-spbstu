034fc0b4785cd9d0d9c8572b758209c815c568eb
Nayzzerr <nastyasly13@gmail.com>
2024-12-15 07:54:00 +0300

    [TASK-9] make var for regexp

--- anastasiya.soboleva/task-9/internal/validation/validation.go
+++ anastasiya.soboleva/task-9/internal/validation/validation.go
@@ -6,8 +6,12 @@ import (
 	"strings"
 )
 
+var (
+	phoneRegex = regexp.MustCompile(`^(\+7|7|8)?\d{10}$`)
+	nameRegex  = regexp.MustCompile(`^[a-zA-Zа-яА-ЯёЁ]+([-\s][a-zA-Zа-яА-ЯёЁ]+)*$`)
+)
+
 func ValidatePhoneNumber(phone string) error {
-	phoneRegex := regexp.MustCompile(`^(\+7|7|8)?\d{10}$`)
 	if !phoneRegex.MatchString(phone) {
 		return errors.New("invalid phone number format")
 	}
@@ -21,7 +25,6 @@ func ValidateName(name string) error {
 		return errors.New("name must be between 2 and 100 characters")
 	}
 
-	nameRegex := regexp.MustCompile(`^[a-zA-Zа-яА-ЯёЁ]+([-\s][a-zA-Zа-яА-ЯёЁ]+)*$`)
 	if !nameRegex.MatchString(name) {
 		return errors.New("name contains invalid characters")
 	}
