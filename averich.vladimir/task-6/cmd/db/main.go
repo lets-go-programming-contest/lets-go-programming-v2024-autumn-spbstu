2df8379e37d3c192bc5d63f8f222bb9aa132001a
aver1ch <vaverich3@inbox.ru>
2024-12-16 06:48:42 +0300

    [TASK-6] Without linters yet

---
+++ averich.vladimir/task-6/cmd/db/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	dbPack "task-6/internal/db"
+)
+
+func main() {
+	connStr := "user=username dbname=mydb sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dbService := dbPack.New(db)
+
+	names, err := dbService.GetNames()
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
