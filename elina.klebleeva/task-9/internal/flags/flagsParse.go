5dcec47fad58c27f37b70311459bc29ce511508c
ElinaKl <lilo.puf@yandex.ru>
2024-12-16 18:09:32 +0300

    [TASK-9] delete bad path to config

--- elina.klebleeva/task-9/internal/flags/flagsParse.go
+++ elina.klebleeva/task-9/internal/flags/flagsParse.go
@@ -4,7 +4,7 @@ import "flag"
 
 func ConfigParse() string {
 	var ConfigPathFlag string
-	flag.StringVar(&ConfigPathFlag, "config", "D:\\golang\\lets-go-programming-v2024-autumn-spbstu\\elina.klebleeva\\task-9\\configs\\config.yml", "Path to YAML config")
+	flag.StringVar(&ConfigPathFlag, "config", "..\\..\\configs\\config.yml", "Path to YAML config")
 	flag.Parse()
 
 	return ConfigPathFlag
